21ab137255ab8965194110593250d23b491f0c67
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-07-22 11:44:02 -0300

    editing comments

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,14 +1,6 @@
 import './styles.css';
 
-// o React é baseado em componentização, estratégia para reaproveitar código, facilitar manutenção e organizar a aplicação
-
-/*
-    Desestruturando o props:
-    - function Card({ name, time })
-    - <strong>{name}</strong>
-    - <small>{time}</small>
-*/
-export function Card(props) { // acessando as propriedades através de props
+export function Card(props) { // acessando as propriedades através do props
     return (
         <div className="card">
             <strong>{props.name}</strong>
@@ -16,11 +8,3 @@ export function Card(props) { // acessando as propriedades através de props
         </div>
     )
 }
-
-/*
-    Imutabilidade
-    - é o princípio que os estados do React respeitam
-    - faz parte do paradigma da programação funcional
-    - diz que o conteúdo não deve ser alterado, mas sim substituído, porque é mais performático
-    - por isso temos uma função para atualizar o conteúdo do estado, e não atualizamos o estado diretamente
-*/
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,27 +1,12 @@
 import React, { useState, useEffect } from 'react'; // useState é o hook que permite a criação de estados
-/* Hooks são funções que permitem conectar os recursos de estado e ciclo de vida do React a partir de componentes totalmente funcionais
-  Normalmente os Hooks iniciam com a palavra use por convenção.
-*/
 
 import './styles.css';
 
 import { Card } from '../../components/Card';
 
 export function Home() {
-  /*
-    Como armazenar e recuperar valores do estado
-
-    Diferença do estado para uma variável comum:
-      - para utilizar o conteúdo de uma variável para refletir na interface o novo conteúdo, é necessário usar estado
-      - o estado, toda vez que muda, vai gerar uma nova renderização da interface
-  */
 
   const [studentName, setStudentName] = useState('');
-  /*
-    - um estado tem dois elementos, onde será guardado o conteúdo do estado, e a função que atualiza esse estado
-    - o nome da função por padrão começa com set seguido do nome do estado
-    - o valor passado dentro da função useState() é o valor inicial do estado
-  */
 
   const [students, setStudents] = useState([]); // armazenar os estudantes da lista de presença
   
@@ -37,53 +22,27 @@ export function Home() {
       })
     };
 
-    // recuperar os estudantes que já estavam no estado antes, criar um novo vetor juntando o que tinha antes na lista e adicionando o novo estudante
-    setStudents(prevState => [...prevState, newStudent]); // adicionando o novo estudante ao estado setStudents
-    // Necessário colocar ... no prevState para tirar o conteúdo do estado anterior de dentro do vetor e despejar dentro do novo vetor
+    // recuperar os estudantes que já estavam no estado antes, criar um novo vetor juntando o que tinha antes na lista e adicionar o novo estudante ao estado setStudents
+    setStudents(prevState => [...prevState, newStudent]);
+    // necessário colocar ... no prevState para tirar o conteúdo do estado anterior de dentro do vetor e despejar dentro do novo vetor
   }
 
-  /*
   useEffect(() => { // o useEffect é executado automaticamente assim que a interface é renderizada
-    // corpo do useEffect: dentro do corpo estão as ações a serem executadas
-    fetch('https://api.github.com/users/madalena-rocha')
-    .then(response => response.json())
-    .then(data => {
-      setUser({ // salvando as informações que a url entregou dentro do estado de user
-        name: data.name,
-        avatar: data.avatar_url,
-      });
-    });
-  },[]) /* array de dependências: serve para colocar os estados que o useEffect depende
-      caso o array de dependências esteja vazio, o userEffect será executado uma única vez no carregamento da tela
-      se dentro do array de dependências tiver um estado como dependência, toda vez que o estado for atualizado, o useEffect será executado também */
-  
-  /*
-    Como lidar com requisições assíncronas dentro do userEffect
-    - o useEffect não aceita ser assíncrono como conseguimos numa função
-    - para ser assíncrono, podemos criar uma função dentro do useEffect, utilizar o async/await na função e chamar a função
-  */
-
-  useEffect(() => {
     async function fetchData() {
       const response = await fetch('https://api.github.com/users/madalena-rocha');
       const data = await response.json();
       console.log("DADOS ===> ", data);
 
-      setUser({
+      setUser({ // salvando as informações que a url entregou dentro do estado de user
         name: data.name,
         avatar: data.avatar_url,
       });
     }
 
     fetchData()
-  }, []);
+  }, []); // array de dependências vazio: o userEffect será executado uma única vez no carregamento da tela
 
   return (
-    /*
-      As expressões JSX devem ter um único elemento filho
-      Elementos JSX devem ser embrulhados em uma tag delimitadora
-      Fragmento JSX <>...</>, <div>...</div> ou qualquer outra tag
-    */
     
     <div className="container">
       <header>
@@ -109,8 +68,8 @@ export function Home() {
       {
         students.map(student => (
         <Card 
-          // Quando temos componentes gerados a partir de uma estrutura de repetição, utilizar chave única para cada componente
-          key={student.time} // geralmente utiliza-se o id
+          // quando temos componentes gerados a partir de uma estrutura de repetição, utilizar chave única para cada componente
+          key={student.time}
           name={student.name} 
           time={student.time} 
         />
@@ -118,18 +77,4 @@ export function Home() {
       }
     </div>
   )
-  /*
-    Componentização:
-    - componente <Card /> criado uma única vez
-    - reaproveitamento do componente <Card />
-    - para passar valor numérico no <Card />, não é necessário utilizar aspas. Ex.: idade={18}
-    - cada componente está sendo renderizado de acordo com as propriedades passadas para cada um
-  */
-
-  /*
-    - percorrer cada estudante da lista
-    - envolver com {} para utilizar o conteúdo do estado
-    - a variável auxiliar student vai armazenar o estudante atual a cada iteração
-    - para cada estudante, renderizar um card
-  */
 }
